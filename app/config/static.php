da91048be26540c3d665a5a78af846ee025e7332
ECWebServices <ecwebservices@gmail.com>
2019-04-06 21:29:15 +0000

    Added MySQL variables to static.php and added the plugins by default

--- app/config/static.php
+++ app/config/static.php
@@ -9,7 +9,16 @@
 $config["sitetitle"] = "EC-Framework";
 
 //The url eg. http://example.com/ (MUST HAVE TRAILING SLASH)
-$config['siteurl'] = "https://ec-framework-ecwebservices.c9users.io/";
+$config['siteurl'] = "https://ecframework.com/";
 
 //The description of the site.
 $config['sitedesc'] = "Site Created by EC-Framework";
+
+//Mysql
+$config['mysqlserver'] = "localhost";
+
+$config['mysqluser'] = "username";
+
+$config["mysqlpass"] = "password";
+
+$config['mysqldb'] = "database";
---
+++ system/plugins.php/example.php
@@ -0,0 +1,4 @@
+<?php
+/**
+ * This is an example plugin to show you how plugins work. You can delete this file if you would like.
+ */
